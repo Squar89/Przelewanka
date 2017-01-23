a23762443bcd9afc24727c792533dff7b56a7dce
Jakub Wróblewski <j.wroblewski97@gmail.com>
2017-01-23 01:03:53 +0100

    Add files via upload

---
+++ przelewanka.ml
@@ -0,0 +1,70 @@
+let przelewanka input_array =
+    let glass =
+        let input_list = Array.to_list input_array in
+        let filter = List.filter
+            (fun (x, _) -> x = 0) input_list in
+        Array.of_list filter
+    in
+    let length = Array.length glass in
+    let rec nwd a b =
+        if b = 0 then a
+        else nwd b (b mod a)
+    in
+    let nwdx =
+        Array.fold_left (fun (x, _) -> nwd (min x a) (max x a)) 0 glass in
+    let test1 = Array.for_all (fun (_, y) -> y mod nwdx = 0) glass in
+    let test2 = Array.exists (fun (x, y) -> (x = y || y = 0)) glass in
+    let yt = Array.init n (fun i -> snd glass.(i)) in
+    let hasht = Hashtbl.create n in
+    let moves = ref 0 in
+    let result = ref 0 in
+    let q = Queue.create in
+    let check_result =
+        if Hashtbl.mem hasht yt then
+            Queue.clear;
+            result := Hashtbl.find hasht yt;
+            true
+        else false
+    in
+    let hasht_update newstate =
+        if not(Hashtbl.mem hasht newstate) then
+            Hashtbl.add hasht newstate (!moves + 1);
+            Queue.push newstate q;
+    in
+    let fill_empty state i =
+        let statec = Array.copy state in
+        statec.(i) <- fst glass.(i);
+        hasht_update statec;
+        statec.(i) <- 0;
+        hasht_update statec;
+    in
+    let pour i j state =
+        let statec = Array.copy state in
+        if state.(j) + state.(i) <= fst glass.(j) then
+            statec.(i) <- 0;
+            statec.(j) <- state.(j) + state.(i);
+            hasht_update statec;
+        else
+            statec.(i) <- state.(i) - (fst glass.(j) - state.(j))
+            statec.(j) <- fst glass.(j);
+            hasht_update statec;
+    in
+    if test1 && test2 then
+        Hashtbl.add hasht (Array.make n 0) 0;
+        Queue.push (Array.make n 0) q;
+        while not(Queue.is_empty q) do
+            let state = Queue.q pop in
+            if not(check_result) then
+                moves := Hashtbl.find hasht state;
+                for i = 0 to (n - 1) do
+                    fill_empty state i;
+                    for j = 0 to (n - 1) do
+                    if not(i = j) then pour i j state;
+                    done                  
+                done
+        done
+        if check_result then
+            !result
+        else -1
+    else if glass = [||] then 0
+        else -1;;
